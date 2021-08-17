5decc8dab273683ff13c28af4a31ab6295bd6e80
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-08-17 14:11:32 -0400

    Update fizzBuzz.js
    still gotta test the code, here's what I have so far

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -30,6 +30,19 @@ function readLine() {
 
 function fizzBuzz(n) {
     // Write your code here
+    // for loop to count from 1 to n (upper limits of test)
+    for (let i = 0; i < n; i++) {
+        // if and else if blocks to guide action/results
+        if (i % 3 === 0 && i % 5 ===0) {
+            console.log("FizzBuzz")
+        } else if (i % 3 === 0) {
+            console.log("Fizz")
+        } else if (i % 5 === 0) {
+            console.log("Buzz")
+        } else {
+            console.log(i);
+        }
+    }
 
 }
 
