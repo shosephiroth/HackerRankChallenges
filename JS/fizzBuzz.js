b59a5fa9ac86de2c7e0b174aa9958abffded7103
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-10-10 18:54:04 -0400

    Update fizzBuzz.js
    not passing yet

--- JS/fizzBuzz.js
+++ JS/fizzBuzz.js
@@ -30,6 +30,17 @@ function readLine() {
 
 function fizzBuzz(n) {
     // Write your code here
+    for (let i = 0; i <= n; i++) {
+        if (n % 3 === 0 && n % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (n % 3 === 0) {
+            console.log("Fizz");
+        } else if (n % 5 === 0) {
+            console.log("Buzz")
+        } else {
+            console.log(n);
+        }
+    }
 
 }
 
