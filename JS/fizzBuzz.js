f221fa8aa6eb11337b6f2bdb7c67cbd6c7c721d0
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-10-10 18:57:03 -0400

    Update fizzBuzz.js
    couple of syntax mistakes

--- JS/fizzBuzz.js
+++ JS/fizzBuzz.js
@@ -30,15 +30,15 @@ function readLine() {
 
 function fizzBuzz(n) {
     // Write your code here
-    for (let i = 0; i <= n; i++) {
-        if (n % 3 === 0 && n % 5 === 0) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
             console.log("FizzBuzz");
-        } else if (n % 3 === 0) {
+        } else if (i % 3 === 0) {
             console.log("Fizz");
-        } else if (n % 5 === 0) {
-            console.log("Buzz")
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
         } else {
-            console.log(n);
+            console.log(i);
         }
     }
 
