9c504e4e0bad95a9e997fefa7bd03b8f636b2223
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-10-10 18:46:19 -0400

    Create fizzBuzz.js
    second attempt at this. Already solved but forgot to do the initial commit first. Initial commit

---
+++ JS/fizzBuzz.js
@@ -0,0 +1,36 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+/*
+ * Complete the 'fizzBuzz' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+function fizzBuzz(n) {
+    // Write your code here
+
+}
+
+function main() {
